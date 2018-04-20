30f442fac4be0616efc46f64183889e0c3e2fec0
vberezny <v.bereznyakov@gmail.com>
2018-04-19 22:34:37 -0700

    Implemented Binary Tree, BST Next

---
+++ Trees/BinaryTree.py
@@ -0,0 +1,44 @@
+class Node:
+
+    def __init__(self, data):
+
+        self.left = None
+        self.right = None
+        self.data = data
+
+
+    def insert(self, data):
+# Compare the new value with the parent node
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+# Print the tree
+    def PrintTree(self):
+        if self.left:
+            self.left.PrintTree()
+        print( self.data),
+        if self.right:
+            self.right.PrintTree()
+
+# initializes tree root
+root = Node(12)
+
+# inserts nodes
+root.insert(6)
+root.insert(14)
+root.insert(3)
+root.insert(18)
+root.insert(21)
+
+root.PrintTree()
